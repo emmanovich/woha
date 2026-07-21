57db99c7fcefe941e24376775c734fdea65088f2
emmanovich <emmanovich@ukr.net>
2026-07-21 21:16:55 +0300

    Create sampledata.go

---
+++ sampledata.go
@@ -0,0 +1,29 @@
+package main
+
+var Names = []string{
+
+	"John Miller",
+
+	"Emma Wilson",
+
+	"Olivia Brown",
+
+	"Daniel Clark",
+
+	"Sophia Taylor",
+
+}
+
+var Streets = []string{
+
+	"Oak Street",
+
+	"River Road",
+
+	"Hill Avenue",
+
+	"Lake Drive",
+
+	"Forest Lane",
+
+}
